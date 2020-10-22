724a95679f9a79b6196d34ea7275073dbb48c835
Nicholas Levenson <nicklevenson@gmail.com>
2020-10-22 12:19:38 -0700

    commit tree a9a552a92c4c4a186380c4a3a1dfcd8099355ff0

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,34 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+# def key_for_min_value(name_hash)
+#     min = Float::INFINITY
+#     returnItem = nil
+#     name_hash.each do |item, price|
+#         if price < min 
+#             min = price
+#             returnItem = item;
+#         end
+#     end
+#     returnItem
+# end
+
 def key_for_min_value(name_hash)
-    min = Float::INFINITY
-    returnItem = nil
-    name_hash.each do |item, price|
-        if price < min 
-            min = price
-            returnItem = item;
+    if name_hash == {}
+        nil
+    end
+
+    integers = []
+    name_hash.collect do |name, number| 
+        integers << number
+        integers = integers.sort do |a, b|
+                a <=> b
         end
     end
-    returnItem
+    name_hash.each do |name, number|
+        if number == integers[0]
+            return name
+        else nil
+        end
+    end
 end
